3db5990f556fd3c83648618e6045cb7df8d389eb
Vaibhav Pant <pantvaibhav16@gmail.com>
2025-05-09 18:46:24 +0530

    removed swagger

--- healthcare_backend/healthcare_backend/urls.py
+++ healthcare_backend/healthcare_backend/urls.py
@@ -6,19 +6,6 @@ from api.views import (
     PatientDoctorMappingByPatientView, LoginView
 )
 from rest_framework_simplejwt.views import TokenRefreshView
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Healthcare API",
-        default_version='v1',
-        description="API documentation for the healthcare backend",
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,7 +27,4 @@ urlpatterns = [
     path('api/mappings/', MappingListCreateView.as_view(), name='mappings'),
     path('api/mappings/<int:pk>/', MappingDetailView.as_view(), name='mapping-detail'),  # For DELETE
     path('api/mappings/patient/<int:patient_id>/', PatientDoctorMappingByPatientView.as_view(), name='mappings-by-patient'),
-
-    # Swagger Docs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
